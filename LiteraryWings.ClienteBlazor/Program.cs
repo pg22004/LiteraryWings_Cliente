c5e7fa94331dd852076fda5c9e8530a09b45ee8c
Axel León <lq22001@esfe.agape.edu.sv>
2023-09-21 15:05:26 -0600

    Codificacion de la api de program

--- LiteraryWings.ClienteBlazor/Program.cs
+++ LiteraryWings.ClienteBlazor/Program.cs
@@ -8,7 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7204/api/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://www.LiteraryWings.somee.com/api/") });
 builder.Services.AddScoped<ILibroService, LibroService>();
 
 builder.Services.AddMudServices(); //Importación de los servicios de MudBlazor
