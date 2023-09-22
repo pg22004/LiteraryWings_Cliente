3fb26db612c99983049676fe42fbd0cde4ac66ab
Iliana <pg22004@esfe.agape.edu.sv>
2023-09-22 09:42:01 +0200

    Correcion de errores en Program.cs

--- LiteraryWings.ClienteBlazor/Program.cs
+++ LiteraryWings.ClienteBlazor/Program.cs
@@ -11,11 +11,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://www.LiteraryWings.somee.com/api/") });
 builder.Services.AddScoped<ILibroService, LibroService>();
 builder.Services.AddScoped<ISugerenciaService, SugerenciaService>();
-<<<<<<< HEAD
 builder.Services.AddScoped<IEditorialService, EditorialService>();
-=======
 builder.Services.AddScoped<ICategoriaService, CategoriaService>();
->>>>>>> 9af8463d861d433401d11622279722c4815900c7
 
 builder.Services.AddMudServices(); //Importación de los servicios de MudBlazor
 
